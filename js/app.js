5de2769d3c394f6230d047bbfcf5559fa0584f31
KuzyaBaka <youryz58@gmail.com>
2022-11-14 12:57:46 +0300

    edit comment to app.js

--- js/app.js
+++ js/app.js
@@ -1,10 +1,16 @@
+// Первое задание:
+
 const text = prompt("Введите текст");
 alert(text);
+
+// Ворое задание:
 
 const numb1 = prompt(`Введите первое число`);
 const numb2 = prompt(`Введите второе число`);
 const numb3 = prompt(`Введите третье число`);
 console.log(Number(numb1) + Number(numb2) + Number(numb3))
+
+// Третье задание:
 
 const userName = prompt(`Введите имя пользователя`)
 const age = prompt(`Ваш возраст`)
@@ -13,3 +19,6 @@ if (age >= 18) {
 } else {
     alert (`Доступ пользователю ${userName} закрыт`)
 }
+
+// Четвёртое задание:
+
